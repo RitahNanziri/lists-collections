a3747a6d757f9732486462b8bea669174cf2f5ac
RitahNanziri <142292629+RitahNanziri@users.noreply.github.com>
2023-09-19 13:16:44 +0300

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,24 @@
+#students_mark
+students_marks=[60,80,90,98]
+students_marks[1]=89
+print(students_marks)
+
+#add a new item 55 
+students_marks=[60,89,90,98]
+students_marks.append(55)
+print (students_marks)
+
+#find the size of the list having 
+print(len(students_marks))
+
+#write a python program to sum all items in the list
+print(sum(students_marks))
+
+#write a python progam function that takes two lists and returns, if they have atleast one common item
+group1=input("enter items")
+group2=input("enter items")
+print ("True,  there is a common item in group1 and group2")
+
+
+
+
